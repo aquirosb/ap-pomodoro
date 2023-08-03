51a88434d69c51675e0028696734423295231a58
aquirosb <aquiros66@gmail.com>
2023-08-03 15:07:21 -0700

    added js file

---
+++ app.js
@@ -0,0 +1,25 @@
+const pomodoroBtn = document.querySelector('.pomodoro')
+const breakBtn = document.querySelector('.break')
+const startBtn = document.querySelector('.start')
+const pauseBtn = document.querySelector('.pause')
+
+pomodoroBtn.addEventListener('click', pomodoroTimer)
+breakBtn.addEventListener('click', breakTimer)
+startBtn.addEventListener('click', startTimer)
+pauseBtn.addEventListener('click', pauseTimer)
+
+async function pomodoroTimer(){
+
+}
+
+async function breakTimer(){
+
+}
+
+async function startTimer(){
+
+}
+
+async function pauseTimer(){
+    
+}
