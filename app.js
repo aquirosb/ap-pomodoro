39f6a1d0f9634ef98989352e2cab495413708fbe
aquirosb <aquiros66@gmail.com>
2023-08-03 17:00:38 -0700

    added functionality to buttons

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
+const timeDisplay = document.getElementById('time')
 const pomodoroBtn = document.querySelector('.pomodoro')
 const breakBtn = document.querySelector('.break')
 const startBtn = document.querySelector('.start')
 const pauseBtn = document.querySelector('.pause')
+
+const pomodoroMins = 25;
+let pomTime = pomodoroMins * 60;
+const breakMins = 1;
+let breakTime = breakMins * 60;
+
+let interval = null
+
+let pomodoroCounter = 0;
+let breakCounter = 0;
 
 pomodoroBtn.addEventListener('click', pomodoroTimer)
 breakBtn.addEventListener('click', breakTimer)
 startBtn.addEventListener('click', startTimer)
 pauseBtn.addEventListener('click', pauseTimer)
 
-async function pomodoroTimer(){
-
+function pomodoroTimer(){
+    const minutes = Math.floor(pomTime/60);
+    let seconds = pomTime % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    timeDisplay.innerHTML = `${minutes}:${seconds}`;
+    pomTime--;
+    pomodoroCounter = 1;
+    breakCounter = 0;
+    if(pomTime < 0){
+        clearInterval(interval)
+        timeDisplay.innerHTML = `00:00`;
+    } 
 }
 
-async function breakTimer(){
-
+function breakTimer(){
+    const minutes = Math.floor(breakTime/60);
+    let seconds = breakTime % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    timeDisplay.innerHTML = `${minutes}:${seconds}`;
+    breakTime--;
+    pomodoroCounter = 0;
+    breakCounter = 1;
+    if(breakTime < 0){
+        clearInterval(interval)
+        timeDisplay.innerHTML = `00:00`;
+    } 
 }
 
-async function startTimer(){
-
+function startTimer(){
+    if(pomodoroCounter == 1 && breakCounter == 0) 
+        interval = setInterval(pomodoroTimer, 1000)
+    else if(pomodoroCounter == 0 && breakCounter == 1)
+        interval = setInterval(breakTimer, 1000)
 }
 
-async function pauseTimer(){
-    
+function pauseTimer(){
+    clearInterval(interval);
+    interval = null;
 }
