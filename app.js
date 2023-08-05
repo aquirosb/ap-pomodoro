987d60ee9beae18127b2a4b93252dd9ec6c7f5e9
aquirosb <aquiros66@gmail.com>
2023-08-04 19:56:55 -0700

    added task bar and displays tasks

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const timeDisplay = document.getElementById('time')
+let taskInput = document.querySelector('#taskInput')
 const pomodoroBtn = document.querySelector('.pomodoro')
 const breakBtn = document.querySelector('.break')
 const startBtn = document.querySelector('.start')
 const pauseBtn = document.querySelector('.pause')
+const addBtn = document.querySelector('.add')
+const deleteBtn = document.querySelector('.delete')
+let tasksOutput = document.querySelector('#tasksOutput')
 
-const pomodoroMins = 2;
+const pomodoroMins = 25;
 let pomTime = pomodoroMins * 60;
-const breakMins = 1;
+const breakMins = 5;
 let breakTime = breakMins * 60;
 
 let interval = null
@@ -14,10 +18,14 @@ let interval = null
 let pomodoroCounter = 0;
 let breakCounter = 0;
 
+var tasks= [""];
+
 pomodoroBtn.addEventListener('click', startPomodoro)
 breakBtn.addEventListener('click', startBreak)
 startBtn.addEventListener('click', startTimer)
 pauseBtn.addEventListener('click', pauseTimer)
+addBtn.addEventListener('click', addTask)
+deleteBtn.addEventListener('click', deleteTask)
 
 function formatTime(minutes, seconds) {
     return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
@@ -33,6 +41,7 @@ function pomodoroTimer(){
         pomodoroCounter = 0;
         breakCounter = 1;
         breakTimer()
+        alert('Time is up.')
     } else {
         pomTime--;
     }
@@ -48,6 +57,7 @@ function breakTimer(){
         pomodoroCounter = 1;
         breakCounter = 0;
         pomodoroTimer()
+        alert('Time is up.')
     } else {
         breakTime--;
     }
@@ -83,4 +93,18 @@ function startTimer(){
 function pauseTimer(){
     clearInterval(interval);
     interval = null;
+}
+
+function addTask(){
+    const newTask = taskInput.value.trim()
+    tasks.push(newTask)
+    taskInput.value = ''
+    tasksOutput.innerHTML = tasks.slice(1).map((task, index) => `<div>${index + 1}. ${task}</div>`).join('')
+}
+
+function deleteTask(){
+    if(tasks.length > 0){
+        tasks.shift();
+        tasksOutput.innerHTML = tasks.slice(1).map((task, index) => `<div>${index + 1}. ${task}</div>`).join('')
+    }
 }
